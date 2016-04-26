ba6ac6eb0c141b8c348c6e23a3a833ba5a833ec9
Simon Hajjar <simon.j.hajjar@gmail.com>
2016-04-26 13:22:57 -0700

    move html to index.html

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 
 const port = 3000;
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    res.sendfile(__dirname + '/public/index.html');
 });
 
 app.listen(port, function () {
-  console.log('Example app listening on port ' + port + '!');
+    console.log('Example app listening on port ' + port + '!');
 });
