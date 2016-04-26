58887cf892194234f6ef2e6db46ab980d75ee819
Simon Hajjar <simon.j.hajjar@gmail.com>
2016-04-26 13:34:05 -0700

    added jquery

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const app = express();
 
 const port = 3000;
 app.use(express.static(__dirname + '/public'));
+app.use('/bower_components', express.static('bower_components')); 
 
 app.get('/', function (req, res) {
     res.sendfile(__dirname + '/public/index.html');
 });
 
 app.listen(port, function () {
-    console.log('Example app listening on port ' + port + '!');
+    console.log('z-9 listening on port ' + port + '.');
 });
