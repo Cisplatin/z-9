12fb8e341ff659eef8502fc625501172bec81d80
Simon Hajjar <simon.j.hajjar@gmail.com>
2016-04-26 16:53:08 -0700

    started add handling

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ app.use('/bower_components', express.static('bower_components'));
 const HASH_LENGTH = 5;
 var maps = {};
 
+app.get('/*', function(req, res, next) {
+    console.log(maps);
+    if(req.url.substr(1) in maps) {
+        res.redirect(maps[req.url.substr(1)]);
+    } else {
+        next();
+    }
+});
+
 app.get('/', function (req, res) {
     res.sendfile(__dirname + '/public/index.html');
 });
@@ -17,11 +26,17 @@ app.get('/add/:url', function(req, res) {
     // TODO Make sure that the generated string doesn't conflict
     // TODO Add an expiry date to each URL
     // TODO Allow for custom URL names
-    if(!(req.params.url in maps)) {
-        maps[req.params.url] = cryptohat().substring(0, HASH_LENGTH);
-    }
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end(maps[req.params.url]);
+    // TODO Check for valid URL
+    // TODO Remove URLs when too many are stored
+    // TODO Handle invalid/expired links
+    // TODO Handle empty request bodies
+    var url = req.body.url;
+    var shrunk = cryptohat().substr(0, HASH_LENGTH);
+    maps[shrunk] = url;
+    res.writeHead(200, {
+        "Content-Type": "text/plain"
+    });
+    res.end(shrunk);
 });
 
 app.listen(port, function () {
