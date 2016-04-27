f1fb9fc672c5ed27547e49908312f4b399e72467
Simon Hajjar <simon.j.hajjar@gmail.com>
2016-04-26 18:46:41 -0700

    silenced dos protection

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + '/public'));
 app.use('/bower_components', express.static('bower_components'));
 app.use(bodyParser.urlencoded({extended: true}));
 
-const ddos = new Ddos;
+const ddos = new Ddos({'silentStart' : true});
 app.use(ddos.express);
 
 const HASH_LENGTH = 5;
