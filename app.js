f5b728da443448b14e74d1e3b2042ba1067a62d4
Simon Hajjar <simon.j.hajjar@gmail.com>
2016-04-26 17:13:45 -0700

    AJAX processing

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const cryptohat = require('cryptohat');
+
 const app = express();
 
 const port = 3000;
 app.use(express.static(__dirname + '/public'));
 app.use('/bower_components', express.static('bower_components'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 const HASH_LENGTH = 5;
 var maps = {};
 
 app.get('/*', function(req, res, next) {
-    console.log(maps);
     if(req.url.substr(1) in maps) {
         res.redirect(maps[req.url.substr(1)]);
     } else {
@@ -22,7 +24,7 @@ app.get('/', function (req, res) {
     res.sendfile(__dirname + '/public/index.html');
 });
 
-app.get('/add/:url', function(req, res) {
+app.post('/add', function(req, res) {
     // TODO Make sure that the generated string doesn't conflict
     // TODO Add an expiry date to each URL
     // TODO Allow for custom URL names
@@ -31,6 +33,7 @@ app.get('/add/:url', function(req, res) {
     // TODO Handle invalid/expired links
     // TODO Handle empty request bodies
     var url = req.body.url;
+    console.log(req.body);
     var shrunk = cryptohat().substr(0, HASH_LENGTH);
     maps[shrunk] = url;
     res.writeHead(200, {
